97e60a5d160963de0a31c591b90fdc39cae52cea
Filip Basta <fbasta7@gmail.com>
2021-06-10 19:39:29 +0200

    Replace custom form with material-ui

--- client/pages/app/register.js
+++ client/pages/app/register.js
@@ -1,63 +1,58 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
 import styles from '../../styles/Register.module.scss';
-import {useForm} from 'react-hook-form';
-import cx from 'classnames';
+import { useForm, Controller } from 'react-hook-form';
 
-export default function Register() {
-  const {register, handleSubmit, formState: {errors}} = useForm({mode: 'onBlur'});
-  const [isFocused, setIsFocused] = useState(false);
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-  };
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#28A745FF',
+        },
+    },
+});
 
-  const onFocus = () => {
-    console.log('sss');
+export default function Register () {
+    const {
+        register,
+        handleSubmit,
+        control,
+        reset,
+        formState: { errors },
+    } = useForm(
+        { mode: 'onBlur' });
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    };
 
-    setIsFocused(true);
-  };
+    const email = register('email', { required: true });
 
-  const onBlur = () => {
-    console.log('aaaa');
-    setIsFocused(false);
-  };
+    return (
+        <div className={styles.register}>
+            <ThemeProvider theme={theme}>
+                <div className={styles.register__wrapper}>
+                    <h2 className={styles.register__heading}>Sign In</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}
+                          className={styles.form}>
+                        <Controller
+                            name="MyCheckbox"
+                            control={control}
+                            defaultValue=""
+                            rules={{ required: true }}
+                            render={({ field }) => <TextField
+                                id="standard-basic" label="Email"
+                                size="normal" {...field} />}
+                        />
+                        {errors.email && <p>This is required</p>}
 
-  const email = register('email', {required: true});
-
-  return (
-      <div className={styles.register}>
-
-        <div className={styles.register__wrapper}>
-          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <div className={styles.form__group}>
-              <label className={cx(
-                  styles.form__group, {
-                    [styles.inputFocus]: isFocused,
-                  },
-              )} htmlFor="email">Email</label>
-              <input onFocus={onFocus} className={styles.form__input} onChange={(e) => {email.onChange(e);}}
-                     onBlur={() => {
-                       email.onBlur;
-                       onBlur();
-                     }}
-                     ref={email.ref}/>
-              {errors.email && <p>This is required</p>}
-            </div>
-            <div className={styles.form__group}>
-              <label htmlFor="">Email</label>
-              <input {...register('pass', {required: true})} />
-            </div>
-
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register('exampleRequired', {required: true})} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
-
-            <input type="submit"/>
-          </form>
+                        <Button className={styles.form__submit} type="submit"
+                                variant="contained" color="primary"
+                                size="large">LOG IN</Button>
+                    </form>
+                </div>
+            </ThemeProvider>
         </div>
-      </div>
-  );
+    );
 }
 
