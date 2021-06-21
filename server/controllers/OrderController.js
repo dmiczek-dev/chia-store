e28a5b11938e0c4fc0f8f48231475ce120782c14
Dawid Miczek <dmiczek52@gmail.com>
2021-06-21 14:39:51 +0200

    added new data to orders

--- server/controllers/OrderController.js
+++ server/controllers/OrderController.js
@@ -63,12 +63,19 @@ exports.createOrder = (req, res) => {
     const orderStatusId = req.body.order_status_id;
     const orderTypeId = req.body.order_type_id;
     const currentDate = new Date();
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const company = req.body.company;
+    const city = req.body.city;
+    const street = req.body.street;
+    const NIP = req.body.NIP;
+    const phone = req.body.phone;
 
     const token = req.cookies.JWT
     const payload = jwt.decode(token);
 
-    client.query("INSERT INTO orders(transaction_id, plots, price, date, pool_key, farmer_key, user_id, order_status_id, order_type_id) VALUES(NULL, $1, $2, $3, $4, $5, $6, $7, $8)",
-        [plots, price, currentDate, poolKey, farmerKey, payload.userId, orderStatusId, orderTypeId]).then(() => {
+    client.query("INSERT INTO orders(transaction_id, plots, price, date, pool_key, farmer_key, user_id, order_status_id, order_type_id, fistname, lastname, company, city, street, NIP, phone) VALUES(NULL, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)",
+        [plots, price, currentDate, poolKey, farmerKey, payload.userId, orderStatusId, orderTypeId, firstname, lastname, company, city, street, NIP, phone]).then(() => {
             res.status(200).send();
         }).catch((err) => {
             res.status(500).send({ error: err })
@@ -86,12 +93,19 @@ exports.editOrder = (req, res) => {
     const farmerKey = req.body.farmer_key;
     const orderTypeId = req.body.order_type_id;
     const currentDate = new Date();
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const company = req.body.company;
+    const city = req.body.city;
+    const street = req.body.street;
+    const NIP = req.body.NIP;
+    const phone = req.body.phone;
 
     const token = req.cookies.JWT
     const payload = jwt.decode(token);
 
-    client.query("UPDATE orders SET plots = $1, price = $2, date = $3, pool_key = $4, farmer_key = $5, order_type_id = $6 WHERE user_id = $7 AND order_id = $8 AND order_status_id IN (SELECT order_status_id FROM order_status WHERE name = 'NOWE')",
-        [plots, price, currentDate, poolKey, farmerKey, orderTypeId, payload.userId, orderId]).then(() => {
+    client.query("UPDATE orders SET plots = $1, price = $2, date = $3, pool_key = $4, farmer_key = $5, order_type_id = $6, firstname = $7, lastname = $8, company = $9, city = $10, street = $11, NIP = $12, phone = $13 WHERE user_id = $14 AND order_id = $15 AND order_status_id IN (SELECT order_status_id FROM order_status WHERE name = 'NOWE')",
+        [plots, price, currentDate, poolKey, farmerKey, orderTypeId, firstname, lastname, company, city, street, NIP, phone, payload.userId, orderId]).then(() => {
             res.status(200).send();
         }).catch((err) => {
             res.status(500).send({ error: err })
