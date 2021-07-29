3cf12599f55b8f2c616c4c5c4f32e727dd44fb0a
Filip Basta <fbasta7@gmail.com>
2021-07-29 14:36:51 +0200

    Move stepper card styles to seperate file

--- client/components/StepperCard/StepperCard.js
+++ client/components/StepperCard/StepperCard.js
@@ -1,77 +1,12 @@
-import React, { useState } from 'react';
-import {} from './StepperCard.styles';
-import { Button } from '@material-ui/core';
+import React  from 'react';
+import { CardIconTitle, CardIconWrapper, StepperCardWrapper, StyledInput } from './StepperCard.styles';
 
-import styles, { css } from 'styled-components';
-import FaceIcon from '@material-ui/icons/Face';
-import styled from 'styled-components';
-import Icon from '@material-ui/core/Icon';
-
-const StepperCardWrapper = styled.div`
-  margin-top: 50px;
-  padding: 70px 40px 40px;
-  background: rgba(40, 167, 69, 0.25);
-  border-radius: 10px;
-  position: relative;
-  min-width: 280px;
-
-  opacity: 0.8;
-  filter: grayscale(.8);
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  ${props => props.isSelected && css`
-    opacity: 1;
-    filter: none;
-    `};
-`;
-
-
-const CardIconWrapper = styled(Icon)`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  margin: 0 auto;
-  width: 100px;
-  height: auto;
-  background-color: rgba(28, 116, 48, 1);
-  padding: 15px;
-  border-radius: 80px;
-  display: block;
-  transform: translate(-50%, -50%);
-  
-
-  svg {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-    fill: white;
-    display: block;
-  }
-`;
-
-const CardIconTitle = styled.h2`
-  text-transform: uppercase;
-  color: #2D3142;
-  font-weight: 400;
-  font-size: 22px;
-  text-align: center;
-`;
-
-const StyledInput = styled.input`
-    display: none;
-`
-
-const StepperCard = ( {Icon, title, onChange, value, name,setSelectedValue, selectedValue, price}) => {
+const StepperCard = ( {Icon, title, onChange, value, name, price, getValues}) => {
     const handleChange = () => {
-        setSelectedValue(value);
-
+        onChange(value)
     }
-
     return (
-        <StepperCardWrapper onChange={() => onChange(value)} isSelected={selectedValue === value}>
+        <StepperCardWrapper as="label" htmlFor={title} onClick={handleChange} isSelected={getValues(name) === value}>
             <CardIconWrapper color="primary">
                 <Icon/>
             </CardIconWrapper>
@@ -79,7 +14,7 @@ const StepperCard = ( {Icon, title, onChange, value, name,setSelectedValue, sele
                 {title}
             </CardIconTitle>
             {price ? <p>{price} PLN</p> : null}
-            <StyledInput type="radio" value={value} name={name} />
+            <StyledInput id={title} type="radio" value={value} name={name} />
         </StepperCardWrapper>
     );
 };
---
+++ client/components/StepperCard/StepperCard.styles.js
@@ -0,0 +1,61 @@
+import styled, { css } from 'styled-components';
+import Icon from '@material-ui/core/Icon';
+
+export const StepperCardWrapper = styled.div`
+  margin-top: 50px;
+  padding: 70px 40px 40px;
+  background: rgba(40, 167, 69, 0.25);
+  border-radius: 10px;
+  position: relative;
+  min-width: 280px;
+  opacity: 0.8;
+  filter: grayscale(.8);
+
+  * {
+    pointer-events: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  ${props => props.isSelected && css`
+    opacity: 1;
+    filter: none;
+  `};
+`;
+
+export const CardIconWrapper = styled(Icon)`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  margin: 0 auto;
+  width: 100px;
+  height: auto;
+  background-color: rgba(28, 116, 48, 1);
+  padding: 15px;
+  border-radius: 80px;
+  display: block;
+  transform: translate(-50%, -50%);
+
+
+  svg {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    fill: white;
+    display: block;
+  }
+`;
+
+export const CardIconTitle = styled.h2`
+  text-transform: uppercase;
+  color: #2D3142;
+  font-weight: 400;
+  font-size: 22px;
+  text-align: center;
+`;
+
+export const StyledInput = styled.input`
+  display: none;
+`;
