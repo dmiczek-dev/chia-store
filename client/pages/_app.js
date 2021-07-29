08fa9bf7dbfa9f711d1f84d548b44b8d828daab5
Filip Basta <fbasta7@gmail.com>
2021-07-29 14:49:13 +0200

    Update dependencies

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,16 +2,16 @@ import NextApp from 'next/app';
 import React from 'react';
 import '../public/fonts/fonts.css';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { ThemeProvider as MaterialThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider as MaterialThemeProvider, createTheme  } from '@material-ui/core/styles';
 import { GlobalStyle } from '../styles/GlobalStyle';
 import { StylesProvider } from '@material-ui/core/styles';
 import Dashboard from '../layout/Dashboard/Dashboard';
 import { useRouter } from 'next/router';
-import useSWR, { SWRConfig } from 'swr'
+import { SWRConfig } from 'swr'
 
 const theme = {
     primary: '#f2f2f2',
-    ...createMuiTheme({
+    ...createTheme ({
         palette: {
             primary: {
                 main: '#28A745FF',
