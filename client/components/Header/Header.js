a95638150fc633a6512309e6414142c8f948f85e
Filip Basta <fbasta7@gmail.com>
2021-08-01 20:14:33 +0200

    Add logut

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { HeaderWrapper, LogoWrapper, PageTitle, StyledButton, StyledLogo } from './Header.styles';
 import { Button } from '@material-ui/core';
+import { setAccessToken } from '../../utils/accessToken';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+    const router = useRouter()
+//TODO: Add logout endpoint
     return (
         <HeaderWrapper>
             <LogoWrapper>
@@ -12,7 +16,7 @@ const Header = () => {
                 Tytuł zakładki
             </PageTitle>
             <StyledButton>
-                <Button variant="outlined">Wyloguj</Button>
+                <Button variant="outlined" onClick={ ()=> {setAccessToken(); router.push('/login'); }}>Wyloguj</Button>
             </StyledButton>
         </HeaderWrapper>
     );
