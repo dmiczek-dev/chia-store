d8c6a62c45115574561e2d8885f9b3afc4aef467
fbas7 <filip.basta@spinney.io>
2021-08-29 13:15:37 +0200

    Display all orders for admin

--- client/pages/app/orders.js
+++ client/pages/app/orders.js
@@ -15,6 +15,7 @@ import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Collapse, Grid } from '@material-ui/core';
+import { getUserRole } from '../../utils/accessToken';
 
 //TODO: Refactor
 
@@ -102,7 +103,10 @@ function Row ({ order }) {
 
 const Orders = () => {
     const { setTitle } = useContext(TitleContext);
-    const { data, states, compareState, fetchData } = useFetch(process.env.NEXT_PUBLIC_URL + 'user/orders', true, null, { method: 'GET' }, true);
+
+    console.log(getUserRole());
+    const url = process.env.NEXT_PUBLIC_URL + (getUserRole() === 'ADMIN' ? 'admin/orders' : 'user/orders');
+    const { data, states, compareState, fetchData } = useFetch(url, true, null, { method: 'GET' }, true);
 
     useEffect(() => {
         setTitle('Zamówienia');
